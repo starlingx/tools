a69e6653579416f1b4c1546d2998fb8870cfffc7
Zhixiong Chi <zhixiong.chi@windriver.com>
2021-05-27 23:09:25 -0400

    stx tool: Initial the stx tool for starlingx debian building
    Create the 'stx' tool to implement the build system to support
    the starlingx debian.
    Now it's just a top command skeleton, later we will add the functional
    module to support more, like control|config.
    Meanwhile update the tox to include pep8 and pylint test for stx.
    
    Story: 2008862
    Task: 42391
    
    Change-Id: Ic5ec479ad3e311ecab28e4d5bc53c9debda7cba3
    Signed-off-by: Zhixiong Chi <zhixiong.chi@windriver.com>

---
+++ stx/lib/stx/__init__.py
@@ -0,0 +1,26 @@
+#
+# STX Build System Python Library
+#
+# Copyright (c) 2021 Wind River Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__ = "1.0.0"
+
+
+class STXHandledException(Exception):
+    """
+    The big dilemma for generic stx code is what information to give the user
+    when an exception occurs. Any exception inheriting this base exception class
+    will provide information to the user。
+    """
