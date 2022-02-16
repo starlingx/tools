c81d01716619a47ae5c93b8b383ce3e12da716d9
Charles Short <charles.short@windriver.com>
2022-02-16 16:17:40 +0000

    debian: Remove /var/lib/sbuild/build from tmpfs
    Remove the /var/lib/sbuild/build directory from tmpfs
    since building large packages such as Ceph and the kernel-std
    debian packages will cause the build to fail with the following
    error:
    
    "No space left on device"
    
    Test Plan:
    PASS Build kernel-std or ceph packages succesfully.
    
    Story: 2008846
    Task: 44154
    
    Signed-off-by: Charles Short <charles.short@windriver.com>
    Change-Id: Id20a2a4115255d890b71a00d9a4d95af9cf22873

--- stx/toCOPY/pkgbuilder/setup.sh
+++ stx/toCOPY/pkgbuilder/setup.sh
@@ -9,7 +9,6 @@ cat >>/etc/fstab << EOF
 # For speeding up sbuild/schroot and prevent SSD wear-out
 tmpfs /var/lib/schroot/session        tmpfs uid=root,gid=root,mode=0755 0 0
 tmpfs /var/lib/schroot/union/overlay  tmpfs uid=root,gid=root,mode=0755 0 0
-tmpfs /var/lib/sbuild/build           tmpfs uid=sbuild,gid=sbuild,mode=2770 0 0
 EOF
 
 mount -a
